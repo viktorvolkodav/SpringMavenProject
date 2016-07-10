10144af4539841cb34dcb3819c44ca325bfd70fe
Виктор <@192.168.56.1>
2016-07-11 00:04:19 +0300

    test

--- src/service/AdminPageService.java
+++ src/service/AdminPageService.java
@@ -16,13 +16,12 @@ import model.ParseInfoFromSite;
 public class AdminPageService {
 
 	private BankDao bankDao;
-	
+
 	@Autowired
 	private ParseInfoFromSite parseInfoFromSite;
-	
+
 	private static Logger logger = LogManager.getLogger();
-	
-	
+
 	@Autowired
 	public void setBankingDAO(BankDao bankDao) {
 		this.bankDao = bankDao;
@@ -30,21 +29,42 @@ public class AdminPageService {
 
 	public String updateDB() {
 		logger.info("run");
+
+		return updateDB(parseSiteNBU(), cleanDB());
+	}
+
+	private String updateDB(List<Bank> banks, boolean isCleanDB) {
+		logger.info("run");
+		
+		if (banks == null)
+			return "Наявні проблеми з отриманням інформації з сайту НБУ. База данних не заповнена.";
+		if (isCleanDB == false)
+			return "База данних не була очищена";
+		try {
+			bankDao.updateDB(banks);
+		} catch (Exception ex) {
+			return "Проблеми з заповненням бази данних. База даних не була запаовнена.";
+		}
+		return "База данних заповнена. В ній наявно " + banks.size()
+				+ " банків";
+	}
+
+	private List<Bank> parseSiteNBU() {
+		logger.info("run");
+		
 		List<Bank> banks = new ArrayList<Bank>();
 		try {
 			banks = parseInfoFromSite.parseSiteNBU();
 		} catch (Exception ex) {
-			return "Наявні проблеми з отриманням інформації з сайту НБУ. База данних не заповнена.";
+			return null;
 		}
-		boolean cleanDB = bankDao.cleanDB();
-		if (cleanDB == true)
-			return "База данних не була очищена";
-
-		try {
-			 bankDao.updateDB(banks);
-		} catch (Exception ex) {
-			return "Проблеми з заповненням бази данних. База даних не була запаовнена.";
-		}
-		return "База данних заповнена. В ній наявно " + banks.size() + " банків";
+		return banks;
 	}
+
+	private boolean cleanDB() {
+		logger.info("run");
+		
+		return !bankDao.cleanDB();
+	}
+
 }
